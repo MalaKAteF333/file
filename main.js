a269c36ea7626ffc5c84b42f9a05664f28572f14
MalaKAteF333 <158581050+MalaKAteF333@users.noreply.github.com>
2024-02-07 02:41:41 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+var sentence1 ="Hello there, my name is mohammed";
+var sentence2 ="I like art and music";
+
+function countvowels(sentence)
+  var letters = sentence.split("");
+  var vowels=0;
+  for (var i=0; i< letters.length;i++){
+    if(
+      letters[i]=="a"||
+      letters[i]=="e"||
+      letters[i]=="o"||
+      letters[i]=="u"||
+      letters[i]=="i"
+    )
+  
+  vowels++;
+}
+console.log(vowels);
+
+
+
+var num=Arraysum(arr);
+console.log(num);
+function countwords=(sentence){
+  var sentence=sentence.split(" ");
+  var numberOfwords=sentenceArr.lemgth;
+  console.log(numberOfwords);
+}
+var sentence="plan human resource managment"
+countwords(sentence);
